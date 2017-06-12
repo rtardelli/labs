3a2e19cf2f504c87d8d98b31fbd768b76e150b6b
Jaconé <edufsh@gmail.com>
2017-06-12 06:03:50 -0300

    Material Icons

--- src/app/app.component.js
+++ src/app/app.component.js
@@ -2,7 +2,7 @@
   app.AppComponent =
     ng.core.Component({
       selector: 'labs-app',
-      template: '<h1>Labs Top Secret Project</h1>'
+      template: '<h1>Labs Top Secret Project <i class="material-icons">sentiment_very_satisfied</i></h1>'
     })
     .Class({
       constructor: function() {}
